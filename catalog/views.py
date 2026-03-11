8db851a320bbb8702af2646c6c9c7a8286ffad08
Arsenii <mgke89172@gmail.com>
2026-03-11 11:09:55 +0300

    Revert "fixes"

--- catalog/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-def product_list(req):
-    pass
-
-def product_detail(req):
-    pass
-
--- ecom/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
-
-def index(req):
-    return HttpResponse('Main page')
